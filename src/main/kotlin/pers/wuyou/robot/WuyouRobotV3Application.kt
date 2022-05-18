06e11097d57c71e12326938db42e509884d4e2ce
wuyou <1097810498@qq.com>
2022-05-18 12:48:32 +0800

    更新依赖

--- src/main/kotlin/pers/wuyou/robot/WuyouRobotV3Application.kt
+++ src/main/kotlin/pers/wuyou/robot/WuyouRobotV3Application.kt
@@ -1,11 +1,14 @@
 package pers.wuyou.robot
 
-import love.forte.simboot.autoconfigure.EnableSimbot
+import love.forte.simboot.spring.autoconfigure.EnableSimbot
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import pers.wuyou.robot.music.config.MusicProperties
 
 @EnableSimbot
 @SpringBootApplication
+@EnableConfigurationProperties(MusicProperties::class)
 class WuyouRobotV3Application
 
 fun main(args: Array<String>) {
